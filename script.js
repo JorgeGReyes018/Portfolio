32a80cf4b07cafefbd317cace902847af5951f15
JorgeGReyes <JorgeGReyes018@yahoo.com>
2022-10-30 16:33:43 -0400

    first set of changes after submission
    After submission this is the first and heaviest change.
    
    1. Added javascript file for dropdown menu. Added the css and html properties accordingly.
    
    2. Removed unnecessary bootstrap files and linked CDN instead.
    
    3. changed header to be fixed on top and z index for dropdown menu.
    
    4. linked GIPHY API in my works to the API I just completed for Project 2

---
+++ script.js
@@ -0,0 +1,9 @@
+// ***** DROPDOWN MENU ON TRIGRAM *****
+function dropdownmenu() {
+    var x = document.getElementById("trigramclick");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
