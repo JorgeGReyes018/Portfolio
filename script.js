ec592e949ecba37cd1d9e060c24181453e256233
JorgeGReyes <JorgeGReyes018@yahoo.com>
2022-12-05 17:59:24 -0500

    Swapped nav for new version.
    Swapped nav for new version. I copied my navbar across project 1 and 2 from project 3 new styling. fixed some links and added images

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// ***** DROPDOWN MENU ON TRIGRAM *****
-function dropdownmenu() {
-    var x = document.getElementById("trigramclick");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+// // ***** DROPDOWN MENU ON TRIGRAM *****
+// function dropdownmenu() {
+//     var x = document.getElementById("trigramclick");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
